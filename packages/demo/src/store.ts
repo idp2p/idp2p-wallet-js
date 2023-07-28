3e6b57d9c2c3ae03b3b77811a94fdcd832b2d35c
adem çağlın <adem_cgln1@hotmail.com>
2023-07-28 23:32:46 +0300

    Store

--- packages/demo/src/store.ts
+++ packages/demo/src/store.ts
@@ -1,5 +1,41 @@
 import { WalletStore } from "@idp2p/wallet-react-core";
-export default class LocalWalletStore implements WalletStore{
+
+type PublicKey = {
+    id: string,
+    pk: Uint8Array
+};
+
+type WalletSession = {
+    ttl: number,
+    created_at: number
+};
+
+export type Wallet = {
+    id: string,
+    name: string,
+    next_pk_digest: Uint8Array,
+    next_rec_pk_digest: Uint8Array,
+    salt: Uint8Array,
+    iv: Uint8Array,
+    cipher: Uint8Array,
+    assertions: PublicKey[],
+    authentications: PublicKey[],
+    agreements: PublicKey[],
+    session?: WalletSession,
+};
+
+export type WalletState = {
+    id: string,
+    name: string,
+    next_pk_digest: Uint8Array,
+    next_rec_pk_digest: Uint8Array,
+    assertions: PublicKey[],
+    authentications: PublicKey[],
+    agreements: PublicKey[],
+    session?: WalletSession
+}
+
+export default class LocalWalletStore implements WalletStore {
     async invoke(cmd: string, args: Record<string, unknown>): Promise<unknown> {
         console.log(cmd, args);
         return 0;
